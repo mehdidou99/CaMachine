914cc57610ffb88bae07e568c7081a514b432277
Mehdi Benharrats <mehdibenharrats@gmail.com>
2017-04-05 16:28:15 +0200

    Remove test.ml from repo
    This tiny test file had nothing to do in the repository. A real testing
    directory (possibly with unit tests) will probably be added to the
    project.

--- test.ml
+++
@@ -1,12 +0,0 @@
-
-load "machine.ml";;
-
-#open "machine";;
-
-let context = get_new_default_context();;
-let code = [PUSH(INT 1); PUSH(INT 3); GT];;
-process_code context code;;
-!(context.s);;
-!(context.env);;
-
-#close "machine";;
